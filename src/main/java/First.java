ca696953fb91b03c539a1425bc43f64ff8c5b5d9
jianglianghao <jianglianghao@126.com>
2023-11-24 22:47:18 +0800

    add

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git11");
         System.out.println("hello git");
     }
 }
